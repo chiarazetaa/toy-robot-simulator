dc62345da079c7677ca79abdff771f188bdb36f9
Chiara Zuffi <c.zuffi@cineca.it>
2024-03-28 15:39:32 +0100

    some optimizations

--- src/app/robot-control/robot-control.component.ts
+++ src/app/robot-control/robot-control.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-robot-control',
@@ -10,89 +8,76 @@ import { Subscription } from 'rxjs';
 export class RobotControlComponent {
   public x: number = 0;
   public y: number = 0;
-  public direction: string = '';
+  public direction: string = 'NORTH';
   public report: any;
   public robotPlaced: boolean = false;
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   placeRobot() {
-    this.http.post('http://localhost:3000/place', { x: this.x, y: this.y, direction: this.direction }, { responseType: 'text' }).subscribe({
-      next: () => {
-        console.log('Robot placed successfully');
-        this.robotPlaced = true; // Set robotPlaced to true after placing the robot
-      },
-      error: (error) => {
-        console.error('Errore HTTP: ' + JSON.stringify(error));
-        throw new Error(error);
-      }
-    });
+    // place the robot in the first position (0, 0, NORTH) 
+    let position = { x: this.x, y: this.y, direction: this.direction };
+    console.log('Robot placed successfully', position);
+    this.robotPlaced = true;
   }
 
   moveRobot() {
-    this.http.post('http://localhost:3000/move', {}, { responseType: 'text' }).subscribe({
-      next: () => {
-        console.log('Robot moved successfully');
-        // Update the robot's position based on the direction
-        switch (this.direction) {
-          case 'NORTH':
-            this.y++;
-            break;
-          case 'SOUTH':
-            this.y--;
-            break;
-          case 'EAST':
-            this.x++;
-            break;
-          case 'WEST':
-            this.x--;
-            break;
-          default:
-            console.error('Invalid direction');
-            break;
-        }
-      },
-      error: (error) => {
-        console.error('Errore HTTP: ' + JSON.stringify(error));
-        throw new Error(error);
-      }
-    });
+    // check if the move will cause the robot to go out of bounds
+    if (this.direction === 'NORTH' && this.y < 4) {
+      this.y++;
+    } else if (this.direction === 'SOUTH' && this.y > 0) {
+      this.y--;
+    } else if (this.direction === 'EAST' && this.x < 4) {
+      this.x++;
+    } else if (this.direction === 'WEST' && this.x > 0) {
+      this.x--;
+    } else {
+      console.log('Move ignored: Robot will go out of bounds');
+    }
   }
 
   turnLeft() {
-    this.http.post('http://localhost:3000/left', {}, { responseType: 'text' }).subscribe({
-      next: () => {
-        console.log('Robot turned left successfully');
-      },
-      error: (error) => {
-        console.error('Errore HTTP: ' + JSON.stringify(error));
-        throw new Error(error);
-      }
-    });
+    switch (this.direction) {
+      case 'NORTH':
+        this.direction = 'WEST';
+        break;
+      case 'SOUTH':
+        this.direction = 'EAST';
+        break;
+      case 'EAST':
+        this.direction = 'NORTH';
+        break;
+      case 'WEST':
+        this.direction = 'SOUTH';
+        break;
+      default:
+        console.error('Invalid direction');
+        break;
+    }
   }
 
   turnRight() {
-    this.http.post('http://localhost:3000/right', {}, { responseType: 'text' }).subscribe({
-      next: () => {
-        console.log('Robot turned right successfully');
-      },
-      error: (error) => {
-        console.error('Errore HTTP: ' + JSON.stringify(error));
-        throw new Error(error);
-      }
-    });
+    switch (this.direction) {
+      case 'NORTH':
+        this.direction = 'EAST';
+        break;
+      case 'SOUTH':
+        this.direction = 'WEST';
+        break;
+      case 'EAST':
+        this.direction = 'SOUTH';
+        break;
+      case 'WEST':
+        this.direction = 'NORTH';
+        break;
+      default:
+        console.error('Invalid direction');
+        break;
+    }
   }
 
-  getReport() {
-    this.http.get('http://localhost:3000/report').subscribe({
-      next: (data) => {
-        this.report = data;
-        console.log('Robot report:', this.report);
-      },
-      error: (error) => {
-        console.error('Errore HTTP: ' + JSON.stringify(error));
-        throw new Error(error);
-      }
-    });
+  getReport(x: number, y: number, direction: string) {
+    this.report = { x: x, y: y, direction: direction };
+    console.log('Robot Position: ', this.report);
   }
 }
